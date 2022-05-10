3257e59b32916ad406b4848664d90a32fae4fb09
jcamilofarfan <farfan1976@hotmail.com>
2022-05-10 10:39:43 -0500

    Cambio de puerto y path

---
+++ src/main/java/com/crud/springboot/CRUD/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.crud.springboot.CRUD.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    @RequestMapping
+    @ResponseBody
+    public ResponseEntity<String> function(){
+        return new ResponseEntity<>("Hola Mundo desde controller con hotreload", HttpStatus.OK);
+    }
+}
